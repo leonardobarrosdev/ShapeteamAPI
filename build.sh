03cd648dce45de3e413149f324e1e370b0e1aebd
Leonardo Barros <leonprogramer@gmail.com>
2024-11-05 17:10:47 -0300

    build updated

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+pip install gunicorn==23.0.* uvicorn==0.32.*
 
 # Convert static asset files
 python manage.py collectstatic --no-input
