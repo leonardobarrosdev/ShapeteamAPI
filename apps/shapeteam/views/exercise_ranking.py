0ee1da6127d7f0f76017fd336319a5cb1f9babfb
Leonardo Barros <leonprogramer@gmail.com>
2025-02-03 22:31:45 -0300

    Patner, daytraining and fixtures uodated

--- apps/shapeteam/views/exercise_ranking.py
+++ apps/shapeteam/views/exercise_ranking.py
@@ -1,16 +1,18 @@
+from django.contrib.auth import get_user_model
 from knox.auth import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
 from apps.shapeteam.models import ExerciseRanking
 from apps.shapeteam.serializers import ExerciseRankingSerializer
 
 
-class ExerciseRankingAPIView(APIView):
+class UserRankingAPIView(APIView):
     queryset = ExerciseRanking.objects.all()
     serializer_class = ExerciseRankingSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get_queryset(self):
-        # return self.queryset.objects.
-        ...
+    def get_score_by_user_level(self, level):
+        ranking = self.queryset.objects.filter(user_level=level).order_by('-score')
+        return self.serializer_class(ranking).data
