38f9fe0c3eeb8caa6bd7626aeadfdfd8f594663a
Leonardo Barros <leonprogramer@gmail.com>
2025-02-06 13:42:17 -0300

    Updated
    Improvement: if haven't datas to process the compatibility all users are returned except partners.

--- apps/shapeteam/views/compatibility.py
+++ apps/shapeteam/views/compatibility.py
@@ -1,33 +1,22 @@
 from knox.auth import TokenAuthentication
 from django.contrib.auth import get_user_model
-from django.db.models import Q, QuerySet
-from rest_framework import generics, viewsets
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+from rest_framework import viewsets
 from rest_framework.response import Response
-from apps.shapeteam.models import WeekRoutine, DayTraining, UserPerformanceMetrics
+from apps.shapeteam.models import Connection, UserPerformanceMetrics
 from apps.user.models import Address
-from apps.user.serializers import UserSerializer, SearchSerializer
-
-
-User = get_user_model()
-
-class WeekRoutineCompatibilityViewSet(viewsets.ModelViewSet):
-    def get_queryset(self):
-        user = self.request.user
-        week_routine = DayTraining.objects.filter(
-            routine=WeekRoutine.user
-        )
+from apps.user.serializers import UserSerializer
 
 
 class UserCompatibilityViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
     
     def get_queryset(self):
         """
-        This function defines the custom queryset for the UserCompatibilityViewSet.
-        It filters users based on their IMC, age, level and location.
+        This function defines the custom queryset for the UserCompatibilityViewSet;
+        It filters users based on their IMC, age, level and location;
         End users will be filtered and ordered by performance metrics.
         Return a list of users that match the criteria (completion rate of top to dow).
         """
@@ -36,13 +25,18 @@ class UserCompatibilityViewSet(viewsets.ModelViewSet):
         address = Address.objects.get(user=user)
         address_list = Address.objects.filter(city=address.city)
         users = [ads.user for ads in address_list]
+        existing_partners = Connection.objects.filter(
+                Q(sender=user) | Q(receiver=user)
+            ).values_list('sender', 'receiver')
+        existing_partner_ids = set(
+            [partner_id for partners in existing_partners for partner_id in partners]
+        )
         for usr in users:
             if (imc - usr.get_imc() > 10) and (age - usr.get_age() > 10) or level != usr.level:
                 users.remove(usr)
-        users.remove(user)
-        user_refs = UserPerformanceMetrics.objects.filter(user__in=users).order_by('-completion_rate').values('user')
-        users = User.objects.filter(id__in=user_refs)
-        return users
+        user_refs = UserPerformanceMetrics.objects.exclude(user__in=existing_partner_ids).filter(user__in=users).order_by('-completion_rate').values('user')
+        users = self.queryset.filter(id__in=user_refs)
+        return users if users else self.queryset.exclude(user__in=existing_partner_ids)
     
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
