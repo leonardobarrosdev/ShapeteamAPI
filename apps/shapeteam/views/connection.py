baa78d2d909917680b5e79b607f5922cd4442372
Leonardo <leonardobarrosdw@gmail.com>
2025-02-17 14:53:46 -0300

    Bug solved
    ipdb removed

--- apps/shapeteam/views/connection.py
+++ apps/shapeteam/views/connection.py
@@ -13,7 +13,6 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Q
-import ipdb
 
 
 User = get_user_model()
