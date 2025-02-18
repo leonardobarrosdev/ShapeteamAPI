4f3df345e3907ffa5c02e27b7c8ac91657c80fa6
Leonardo <leonardobarrosdw@gmail.com>
2025-02-18 00:08:54 -0300

    Temporari adjustment for explore

--- apps/shapeteam/views/compatibility.py
+++ apps/shapeteam/views/compatibility.py
@@ -1,6 +1,6 @@
 from knox.auth import TokenAuthentication
 from django.contrib.auth import get_user_model
-from django.db.models import Q
+from django.db.models import Q, Avg, F, FloatField, ExpressionWrapper
 from rest_framework import viewsets
 from rest_framework.response import Response
 from apps.shapeteam.models import Connection, UserPerformanceMetrics
@@ -23,39 +23,47 @@ class UserCompatibilityViewSet(viewsets.ModelViewSet):
         existing_partners = Connection.objects.filter(
                 Q(sender=user) | Q(receiver=user)
             ).values_list('sender', 'receiver')
-        existing_partner_ids = set(
-            [partner_id for partners in existing_partners for partner_id in partners]
-        )
+        existing_partner_ids = [partner_id for partners in existing_partners for partner_id in partners]
         users = self.queryset.exclude(id__in=existing_partner_ids).exclude(id=user.id)
         return users
     
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        # user = self.request.user
-        # address = Address.objects.get(user=user)
-        # if address:
-        #     queryset = self.find_by_address(address)
+    def list(self, request):
+        # address = self.find_by_address()
+        # features = self.find_by_features()
+        # users = address.objects.contains(id__in=features.values('id'))
+        users = self.find_by_address()
+        queryset = users | self.get_queryset()
+        usr = self.find_by_performance(queryset)
+        if usr:
+            queryset = usr
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
     
-    def find_by_address(self, address):
+    def find_by_address(self):
         """Find users by address"""
         user = self.request.user
         queryset = self.get_queryset()
+        address = Address.objects.get(user=user)
         addresses = Address.objects.exclude(user=user).filter(city=address.city)
         users = queryset.filter(id__in=addresses.values('user'))
         return users
 
-    def find_by_features(self, features):
+    def find_by_features(self):
         """It filters users based on their IMC, age and level"""
         user = self.request.user
         queryset = self.get_queryset()
         imc, age, level = user.get_imc(), user.get_age(), user.level
-        users = queryset.filter(
-            Q(imc__gte=imc-1) | Q(imc__lte=imc+1) &
-            Q(age__gte=age-1) | Q(age__lte=age+1) &
-            Q(level=level)
-        )
+        # Calculate median IMC and age
+        # median_imc = queryset.aggregate(
+        #     median_imc=Avg(ExpressionWrapper(F('weight') / (F('height') ** 2),
+        #     output_field=FloatField()))
+        # )['median_imc']
+        # median_age = queryset.aggregate(median_age=Avg('date_birth'))['median_age']
+        # users = queryset.filter(
+        #     Q(imc__gte=median_imc-1) | Q(imc__lte=median_imc+1) &
+        #     Q(age__gte=median_age-1) | Q(age__lte=median_age+1)
+        # )
+        users = queryset.filter(level=level)
         return users
     
     @classmethod
