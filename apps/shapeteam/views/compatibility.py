51f7e75b7d3ac998f2a7bf8a6c8e832a57c12329
Leonardo <leonardobarrosdw@gmail.com>
2025-02-28 15:13:19 -0300

    Filter gym added

--- apps/shapeteam/views/compatibility.py
+++ apps/shapeteam/views/compatibility.py
@@ -1,23 +1,28 @@
+from datetime import datetime
 from knox.auth import TokenAuthentication
 from django.contrib.auth import get_user_model
-from django.db.models import Q, Avg, F, FloatField, ExpressionWrapper
-from rest_framework import viewsets
+from django.db.models import (
+    Q, F, FloatField, IntegerField, ExpressionWrapper, DateField, Value
+)
+from django.db.models.functions import Round, ExtractDay
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from apps.shapeteam.models import Connection, UserPerformanceMetrics
+from apps.shapeteam.models import Connection
 from apps.user.models import Address
 from apps.user.serializers import UserSerializer
+from apps.shapeteam.models import Gym
 
 
 class UserCompatibilityViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
+    queryset = get_user_model().objects.exclude(is_superuser=True)
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
     
     def get_queryset(self):
         """
         This function defines the custom queryset for the UserCompatibilityViewSet;
-        End users will be filtered and ordered by performance metrics.
-        Return a list of users that match the criteria (completion rate of top to dow).
+        Filter users that are not already connected with the current user and exclude the current user;
+        return the queryset.
         """
         user = self.request.user
         existing_partners = Connection.objects.filter(
@@ -28,16 +33,13 @@ class UserCompatibilityViewSet(viewsets.ModelViewSet):
         return users
     
     def list(self, request):
-        # address = self.find_by_address()
-        # features = self.find_by_features()
-        # users = address.objects.contains(id__in=features.values('id'))
-        users = self.find_by_address()
-        queryset = self.get_queryset() if not users else users
-        # usr = self.find_by_performance(queryset)
-        # if usr:
-        #     queryset = usr
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+        user_address = self.find_by_address()
+        user_features = self.find_by_features()
+        queryset = user_address.filter(id__in=user_features)
+        if not queryset:
+            queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def find_by_address(self):
         """Find users by address"""
@@ -52,30 +54,43 @@ class UserCompatibilityViewSet(viewsets.ModelViewSet):
             return None
 
     def find_by_features(self):
-        """It filters users based on their IMC, age and level"""
+        """
+        It filters users based on their IMC, age, level and goal
+        rerun users with similar feactures.
+        """
         user = self.request.user
         queryset = self.get_queryset()
-        imc, age, level = user.get_imc(), user.get_age(), user.level
-        # Calculate median IMC and age
-        # median_imc = queryset.aggregate(
-        #     median_imc=Avg(ExpressionWrapper(F('weight') / (F('height') ** 2),
-        #     output_field=FloatField()))
-        # )['median_imc']
-        # median_age = queryset.aggregate(median_age=Avg('date_birth'))['median_age']
-        # users = queryset.filter(
-        #     Q(imc__gte=median_imc-1) | Q(imc__lte=median_imc+1) &
-        #     Q(age__gte=median_age-1) | Q(age__lte=median_age+1)
-        # )
-        users = queryset.filter(level=level)
+        imc, age, level, goals = user.get_imc(), user.get_age(), user.level, user.goal.all()
+        # Anotate the queryset with the IMC for search user
+        queryset = self.queryset.annotate(
+            age=ExpressionWrapper(
+                ExtractDay(
+                    ExpressionWrapper(
+                        Value(datetime.now()) - F('date_birth'),
+                        output_field=DateField()
+                    )
+                )
+                / 365.25,
+                output_field=IntegerField()
+            ),
+            imc=Round(F('weight') / (F('height') ** 2.0), output_field=FloatField())
+        )
+        # Define the IMC range for filtering
+        age_tolerance, imc_tolerance = 10, 5
+        users = queryset.filter(
+            Q(age__gte=age - age_tolerance, age__lte=age + age_tolerance) |
+            Q(imc__gte=imc - imc_tolerance, imc__lte=imc + imc_tolerance) |
+            Q(age=age) |
+            Q(imc=imc) |
+            Q(level=level) &
+            Q(goal__in=goals)
+        ).distinct()
         return users
     
-    @classmethod
-    def find_by_performance(cls, users):
-        """
-        Find users by performance metrics
-        order by completion rate
-        return a list of users ordered by completion rate
-        """
-        performance_metrics = UserPerformanceMetrics.objects.filter(user__id__in=users)
-        users = performance_metrics.order_by('completion_rate')
-        return users
+    def find_by_gym(self):
+        """Find users by gym location"""
+        queryset = self.get_queryset()
+        user_ids = Gym.objects.get(location__user=self.request.user).values('user')
+        queryset = queryset.filter(id__in=user_ids)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
